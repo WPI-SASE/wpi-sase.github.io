84fa3fc863e50631697c7c0e35f9c3ea573309a2
WP Infusion <info@wpinfusion.com>
2026-03-05 18:00:59 +0100

    additional fixes and optimizations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,21 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-import tailwindcss from "@tailwindcss/vite";
 import alpinejs from "@astrojs/alpinejs";
-import playformInline from "@playform/inline";
 import mdx from "@astrojs/mdx";
+import tailwindcss from "@tailwindcss/vite";
+import playformInline from "@playform/inline";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://astropie.netlify.app",
-	base: "/",
-	// trailingSlash: 'always',
-	integrations: [
-		alpinejs(),
-		playformInline({
-			Beasties: true,
-		}),
-		mdx(),
-	],
-	output: "static",
-	devToolbar: {
-		enabled: false,
-	},
-	vite: {
-		plugins: [tailwindcss()],
-	},
+    site: "https://astropie.netlify.app",
+    base: "/",
+    // trailingSlash: 'always',
+    integrations: [alpinejs(), mdx(), (await import("@playform/inline")).default()],
+    output: "static",
+    devToolbar: {
+        enabled: false,
+    },
+    vite: {
+        plugins: [tailwindcss()],
+    },
 });
